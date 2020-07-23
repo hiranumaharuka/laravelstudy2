75011391046419c61aca920f76ef72d88277e9dc
hiranumaharuka <colorcord251@gmail.com>
2020-07-23 19:19:22 +0900

    ログインしてたら系の処理

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,33 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
+                    <a href="/posts/create" class="btn btn-primary">Create Post</a>
+                    <h3>Your Blog Posts</h3>
 
-                    {{ __('You are logged in!') }}
+
+                    @if (count($posts) > 0)
+
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Title</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach ($posts as $post)
+                           <tr>
+                           <th>{{ $post->title}}</th>
+                               <th><a href="/posts/{{ $post->id }}/edit" class="btn btn-default">Edit</a></th>
+                               <th></th>
+                           </tr>
+                        @endforeach
+                    </table>
+                    @else
+                    <p>You have no posts.</p>
+                    @endif
                 </div>
             </div>
         </div>
